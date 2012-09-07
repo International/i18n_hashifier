19454212e061182ecb7d000dc357ad2fb9df91c7
George Opritescu <ssscripting@gmail.com>
2012-09-07 22:31:29 +0300

    Some debugging

--- lib/i18n_hashifier.rb
+++ lib/i18n_hashifier.rb
@@ -7,9 +7,12 @@ module I18nHashifier
       keys = hash.keys
       keys.each do |key|
 #        binding.pry
+        puts "Working on key #{key}"
         components = key.split(separator)
         accessor = nil
         components.each_with_index do |component,idx|
+          puts "Accessing component class:#{component.class} value:#{component} idx=#{idx}"
+          puts "Accessor: #{accessor.inspect}"
           component_representation = keys_as_symbols ? component.to_sym : component
           is_last = idx == components.size - 1
           if is_last
